e4ed3655acc31c395969c8a41398ec9c351b49ff
ghostBurst <adriencreus59@gmail.comm>
2023-07-18 22:19:36 +0200

    modif

--- Controllers/GymsController.cs
+++ Controllers/GymsController.cs
@@ -22,5 +22,42 @@ namespace GymBookingPlatform.Controllers
             var gyms = _context.Gyms.ToList();
             return Ok(gyms);
         }
+        [HttpPost]
+        public IActionResult Post([FromBody] Gym gym)
+        {
+            _context.Gyms.Add(gym);
+            _context.SaveChanges();
+            return Ok(gym);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Gym gym)
+        {
+            var existingGym = _context.Gyms.Find(id);
+            if (existingGym == null)
+            {
+                return NotFound();
+            }
+
+            existingGym.Name = gym.Name;
+            existingGym.Location = gym.Location;
+            _context.SaveChanges();
+            return Ok(existingGym);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var gym = _context.Gyms.Find(id);
+            if (gym == null)
+            {
+                return NotFound();
+            }
+
+            _context.Gyms.Remove(gym);
+            _context.SaveChanges();
+            return Ok(gym);
+        }
+
     }
 }
